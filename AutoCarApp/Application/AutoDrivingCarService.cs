5d9430a49a864d90de9b964373cda0c68b2b2201
miluka-swivel <miluka@swivelgroup.com.au>
2024-01-06 13:34:58 +0530

    AutoDrivingCarService, AutoDrivingCar Domain entity and Position value object added

---
+++ AutoCarApp/Application/AutoDrivingCarService.cs
@@ -0,0 +1,23 @@
+﻿using AutoCarApp.Domain.Entities;
+using AutoCarApp.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCarApp.Application
+{
+    public class AutoDrivingCarService
+    {
+        public void SimulateAutoDrivingCar(int width, int height, Position initialPosition, string commands)
+        {
+            var car = new AutoDrivingCar(width, height, initialPosition);
+            car.Move(commands);
+
+            // Display the final position and direction
+            var (x, y, direction) = car.Position;
+            Console.WriteLine($"{x} {y} {direction}");
+        }
+    }
+}
